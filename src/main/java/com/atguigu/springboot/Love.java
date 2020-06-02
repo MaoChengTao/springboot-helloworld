7066b8b8aacfcfb8066d1e6263d07e8bf455d8d2
毛成涛 <875122038@qq.com>
2020-06-02 19:23:25 +0800

    增加main

--- src/main/java/com/atguigu/springboot/Love.java
+++ src/main/java/com/atguigu/springboot/Love.java
@@ -3,4 +3,8 @@ package com.atguigu.springboot;
 public class Love {
     int num1 = 1;
     int num2 = 2;
+
+    public static void main(String[] args) {
+        System.out.println("hello Github");
+    }
 }
